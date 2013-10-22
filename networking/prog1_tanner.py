f105846ecf13bb4dc66bbd1d7514ac98fd801cd2
Josh Tanner <mindful.jt@gmail.com>
2013-10-21 17:44:38 -0700

    finish webserver

--- networking/prog1_tanner.py
+++ networking/prog1_tanner.py
@@ -1,33 +1,38 @@
 #This can't bind properly unless it's run as root (or with sudo, obviously)
+import sys
+try:
+	from socket import *
+	serversocket = socket(AF_INET, SOCK_STREAM)
+	serversocket.bind(('', 80)) #empty string means all available interfaces, port 80
+	serversocket.listen(5) #queue up to 5 connect requests
 
-from socket import *
-serversocket = socket(AF_INET, SOCK_STREAM)
-serversocket.bind(('', 80)) #empty string means all available interfaces, port 80
-serversocket.listen(5) #queue up to 5 connect requests
-
-print("Winding up on port 80...")
-print()
-while True:
-	connectionSocket, addr = serversocket.accept() #blocks until we receive a tcp connection
-	print("TCP connection received from:"+addr[0]+" at port:"+str(addr[1]))
+	print("Winding up on port 80...")
 	print()
+	while True:
+		connectionSocket, addr = serversocket.accept() #blocks until we receive a tcp connection
+		print("TCP connection received from:"+addr[0]+" at port:"+str(addr[1]))
+		print()
 
-	try:
-		message = connectionSocket.recv(1024) #Number of bytes to read, I assume
-		filename = message.split()[1]
-		print(filename+" requested...")
-		print()
-		f = open(filename)
-		outputdata = f.read()
-		#Send one HTTP header line into socket
-		for i in range(0, len(outputdata)):
-			connectionSocket.send(outputdata[i]) #Write the outputdata, one character at a time
-		print("...and served")
-		print()
-		connectionSocket.close()
-	except IOError: #File not found
-		print("...but not found")
-		print()
-		#TODO:Send response message for file not found
-		connectionSocket.close()
-serversocket.close()
+		try: #Read, look for file
+			message = connectionSocket.recv(1024) #Number of bytes to read, I assume
+			filename = message.split()[1].decode('UTF-8').strip('/')
+			print(filename+" requested...")
+			print()
+			f = open(filename)
+			outputdata = f.read()
+			connectionSocket.send("HTTP/1.1 200 OK \n\n".encode('UTF-8'))
+			connectionSocket.send(outputdata.encode('UTF-8')) #Write the outputdata, one character at a time
+			print("...and served")
+			print()
+			connectionSocket.close()
+		except IOError: #File not found
+			print("...but not found")
+			connectionSocket.send("HTTP/1.1 404 Not Found \n\n".encode('UTF-8'))
+			print()
+			#TODO:Send response message for file not found
+			connectionSocket.close()
+	serversocket.close()
+except Exception as err:
+	serversocket.close()
+	print("Exception detected, closing socket and reraising...")
+	raise err
