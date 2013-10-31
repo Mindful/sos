9322e9ecea9a0473de02d4231b22caeafffd3618
Josh Tanner <mindful.jt@gmail.com>
2013-10-31 15:09:04 -0700

    work

--- data_structures/project_1/job.cpp
+++ data_structures/project_1/job.cpp
@@ -5,7 +5,7 @@ public:
 	const int procs, totalTicks, id;
 	const string description;
 	job(string job_description, int n_procs, int n_ticks, int job_id) : description(job_description), 
-	procs(n_procs), totalTicks(n_ticks), remainingTicks(n_ticks), id(job_id)
+		procs(n_procs), totalTicks(n_ticks), remainingTicks(n_ticks), id(job_id)
 	{
 	};
 
--- data_structures/project_1/scheduler.cpp
+++ data_structures/project_1/scheduler.cpp
@@ -1,5 +1,9 @@
 #include "scheduler.h"
 
+//todo: what's generating segmentation faults here is very likely attempting to iterate empty queues
+//the for loops that use waitQueue and runQueue should validate that there are objects in those queues
+//before attempting to perform any kind of iteration on them
+
 int getInt(string prompt){
 	int val;
 	string input;
@@ -18,21 +22,25 @@ scheduler::scheduler(int processors, int inputInterval) : freeProcs(processors),
 }
 
 void scheduler::insertJob(string job_description, int n_procs, int n_ticks){
+	//TODO: validate that this isn't more than the processors we have
+	//also validate no negatives
 	job j(job_description, n_procs, n_ticks, ++nextId);
 	waitQueue.push_front(j);
 }
 
-job scheduler::findDelShortest(){
-	int min = -1;
+bool scheduler::findDelShortest(job** buffer){
+	//todo: I think this is iterating backwards, given we want a queue and not a stack.
+	int min = -1; //invalid min, so we know if it hasn't been set
 	for(list<job>::iterator it=waitQueue.begin(); it != waitQueue.end(); ++it){
-		if (!it->tick()) 
+		if (it->procs <= freeProcs && (min == -1 || it->totalTicks < min)) 
 		{
-			//TODO: free processors
-			//TODO: print job completion
+			*buffer = new job(*it); //Copy the saved job into something so we can return it, since erase cleans memory too
 			waitQueue.erase(it); //using the iterator we have makes this O(1)
+			return true;
 		}
 	}
-	//if min is still -1, we didn't find anything, and this is a fail.
+	*buffer = NULL;
+	return false;
 }
 
 void scheduler::getInput()
@@ -51,9 +59,6 @@ void scheduler::getInput()
 			cout << "Unrecognized input. Please try again" << endl;
 		}
 		else{
-
-			//get actual job info, check for validity. use helper function
-			//to loop for valid input on each thing.
 			cout << "Please enter a job description" << endl;
 			getline(cin, buffer);
 			n_procs = getInt("Please enter a number of required processors");
@@ -66,15 +71,29 @@ void scheduler::getInput()
 }
 
 void scheduler::tick(){
-	for(list<job>::iterator it=waitQueue.begin(); it != waitQueue.end(); ++it){
+	job *j; //holds found job, if any
+	while (findDelShortest(&j)){
+		runJob(j); //J copied into runqueue
+		delete j; //to prevent memory leak
+	}
+
+	for(list<job>::iterator it=runQueue.begin(); it != runQueue.end(); ++it){
 		if (!it->tick()) //if this job is finished
 		{
-			//TODO: free processors
-			//TODO: print job completion
-			waitQueue.erase(it); //using the iterator we have makes this O(1)
-		}
+			usedProcs -= it->procs;
+			freeProcs += it->procs;
+			cout << "Job with description \"" << it->description << "\" finished after " 
+				<< it->totalTicks << " ticks on " << it->procs << " processors" << endl;
+			//using the iterator we have makes this O(1)
+			runQueue.erase(it--); //the -- is very important because erase invalidates the iterator
+		};
 	}
-	//traverse the waitQueue. tick on each job, and perform removals if their ticks return false
+}
+
+void scheduler::runJob(job *j){
+	runQueue.push_front(*j);
+	freeProcs-=j->procs;
+	usedProcs+=j->procs;
 }
 
 void scheduler::start()
@@ -84,6 +103,7 @@ void scheduler::start()
 	{
 		if (++i == inputInterval)
 		{
+			cout << inputInterval << " ticks elapsed." << endl;
 			getInput();
 			i = 0;
 		}
